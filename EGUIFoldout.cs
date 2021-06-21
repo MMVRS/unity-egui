df090fcb4aea9d2ac46048ee6845017dd603f8b5
George Markosian <georgemarkosian@gmail.com>
2021-06-21 18:15:36 +0300

    Foldout added.

---
+++ EGUIFoldout.cs
@@ -0,0 +1,58 @@
+#if UNITY_EDITOR
+
+using System;
+using Build1.UnityEGUI.Types;
+using UnityEditor;
+using UnityEngine;
+
+namespace Build1.UnityEGUI
+{
+    public static partial class EGUI
+    {
+        public static int       FoldoutH3FontSize  { get; set; } = 14;
+        public static FontStyle FoldoutH3FontStyle { get; set; } = FontStyle.Bold;
+        public static Color     FoldoutH3Color     { get; set; } = Color.white;
+
+        public static void Foldout(string title, FoldoutType type, ref bool foldout)
+        {
+            var fontSize = type switch
+            {
+                FoldoutType.Default => EditorStyles.foldout.fontSize,
+                FoldoutType.H3      => FoldoutH3FontSize,
+                _                   => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
+
+            var fontStyle = type switch
+            {
+                FoldoutType.Default => EditorStyles.foldout.fontStyle,
+                FoldoutType.H3      => FoldoutH3FontStyle,
+                _                   => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
+
+            var color = type switch
+            {
+                FoldoutType.Default => EditorStyles.foldout.normal.textColor,
+                FoldoutType.H3      => FoldoutH3Color,
+                _                   => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
+
+            var foldoutStyle = new GUIStyle(EditorStyles.foldout)
+            {
+                normal = { textColor = color },
+                onNormal = { textColor = color },
+                hover = { textColor = color },
+                onHover = { textColor = color },
+                active = { textColor = color },
+                onActive = { textColor = color },
+                focused = { textColor = color },
+                onFocused = { textColor = color },
+                fontSize = fontSize,
+                fontStyle = fontStyle,
+                contentOffset = new Vector2(3, 0)
+            };
+            foldout = EditorGUILayout.Foldout(foldout, title, foldoutStyle);
+        }
+    }
+}
+
+#endif
---
+++ Types/FoldoutType.cs
@@ -0,0 +1,12 @@
+#if UNITY_EDITOR
+
+namespace Build1.UnityEGUI.Types
+{
+    public enum FoldoutType
+    {
+        Default = 0,
+        H3      = 3
+    }
+}
+
+#endif
