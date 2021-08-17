7bbe1e188570fec14a63a65799878529bb714191
George Markosian <georgemarkosian@gmail.com>
2021-08-17 13:45:49 +0300

    Flags checkboxes returned.

---
+++ EGUIFlags.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+
+using System;
+using System.Linq;
+
+namespace Build1.UnityEGUI
+{
+    public static partial class EGUI
+    {
+        public static void CheckboxFlags<T>(T value, Action<T, bool> onChange) where T: Enum
+        {
+            Horizontally(() =>
+            {
+                foreach (var flag in System.Enum.GetValues(typeof(T)).Cast<T>())
+                {
+                    Checkbox(flag.ToString(), value.HasFlag(flag), value => onChange(flag, value));
+                }
+            });
+        }
+    }
+}
+
+#endif
